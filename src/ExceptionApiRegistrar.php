8b1f11e1a17e956245c55e04682fbfe974a8f0d1
Ricardo Bazan <ricardo.bazan@suizoargentina.com>
2025-06-11 15:23:52 -0300

    agrego nuevo exception

--- src/ExceptionApiRegistrar.php
+++ src/ExceptionApiRegistrar.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Illuminate\Auth\AuthenticationException;
+
 use Throwable;
 
 class ExceptionApiRegistrar
@@ -29,6 +31,9 @@ class ExceptionApiRegistrar
 
                 if ($e instanceof RouteNotFoundException) {
                     return apisuizo()->unauthorized('Token inválido o no enviado');
+                }
+                if ($e instanceof AuthenticationException) {
+                    return apisuizo()->unauthorized('Token de autorización requerido o inválido');
                 }
             }
 
